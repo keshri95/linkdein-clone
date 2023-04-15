a5611a7e8d39600ca89adebc4bc51503af052c56
Ashish Keshri <keshri.ashish1@gmail.com>
2023-04-15 11:07:45 +0530

    sign-up-page

--- src/components/RegisterComponent.jsx
+++ src/components/RegisterComponent.jsx
@@ -1,52 +1,57 @@
 import React, { useState } from "react";
 import "../Sass/LoginComponent.scss";
-import { LoginAPI, GoogleSignInAPI } from "../api/AuthAPI";
+import { LoginAPI, GoogleSignInAPI, RegisterAPI } from "../api/AuthAPI";
 import LinkdeinLogo from "../assets/link.png"
 import GoogleButton from "react-google-button";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
-export default function LoginComponent() {
-  const [credentials, setCredentials] = useState({});
-  let navigate = useNavigate()
-
-  const login = async () => {
-    try{
-      let res = await LoginAPI(credentials.email, credentials.password);
-      console.log(res?.user);
-      toast.success('Signed in to Linkdein')
+export default function RegisterComponent(){
+    const [credentials, setCredentials] = useState({});
+    let navigate = useNavigate()
+  
+    const login = async () => {
+      try{
+        let res = await RegisterAPI(credentials.email, credentials.password);
+        console.log(res?.user);
+        toast.success('Account Created!')
+        navigate("/home")
+      } catch (err){
+        console.log(err);
+        toast.error("Connot create your Account");
+      }
+  
+    };
+  
+  
+    const googleSignIn = () => {
+      let response = GoogleSignInAPI()
+      console.log(response)
       navigate("/home")
-
-    } catch (err){
-      console.log(err);
-      toast.error("Please Check your Credentials");
     }
-
-  };
-
-
-  const googleSignIn = () => {
-    let response = GoogleSignInAPI()
-    console.log(response)
-    navigate("/home")
-  }
-
   return (
     <div className="login-wrapper">
     <img src={LinkdeinLogo} className="linkedinLogo" />
 
     <div className="login-wrapper-inner">
-      <h1 className="heading">Sign in</h1>
-      <p className="sub-heading">Stay updated on your professional world</p>
+      <h1 className="heading">Make the most of your professional life</h1>
 
       <div className="auth-inputs">
+        <input
+          onChange={(event) =>
+            setCredentials({ ...credentials, name: event.target.value })
+          }
+          type="text"
+          className="common-input"
+          placeholder="Your Name"
+        />
         <input
           onChange={(event) =>
             setCredentials({ ...credentials, email: event.target.value })
           }
           type="email"
           className="common-input"
-          placeholder="Email or Phone"
+          placeholder="Email or phone number"
         />
         <input
           onChange={(event) =>
@@ -54,14 +59,14 @@ export default function LoginComponent() {
           }
           type="password"
           className="common-input"
-          placeholder="Password"
+          placeholder="Password (6 or more characters)"
         />
       </div>
       <button onClick={login} className="login-btn">
-        Sign in
+        Agree & Join
       </button>
     </div>
-    <hr className="hr-text" data-content="or" />
+    <hr class="hr-text" data-content="or" />
     <div className="google-btn-container">
       <GoogleButton className="google-btn" onClick={googleSignIn} />
 
@@ -73,5 +78,5 @@ export default function LoginComponent() {
       </p>
     </div>
   </div>
-  );
+  )
 }
