5af8d30c06b359142d97fed717c49519c45a2527
Ashish Keshri <keshri.ashish1@gmail.com>
2023-04-23 11:20:37 +0530

    post-card

--- src/components/common/PostCard/index.jsx
+++ src/components/common/PostCard/index.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useMemo, useState } from "react";
 import "./index.scss";
+import { useNavigate } from "react-router-dom";
+import LikeButton from "../LikeButton";
+import { getCurrentUser } from "../../../api/FirestoreAPI";
 
-export default function PostCard({posts, id}) {
+export default function PostCard({ posts, id }) {
+  let navigate = useNavigate();
+
+  const [currentUser, setCurrentUser] = useState({})
+
+  useMemo(() => {
+    getCurrentUser(setCurrentUser)
+  }, [])
+  // console.log(currentUser?.userID) // id
   return (
-    <div className='posts-card' key={id}>
-        <p className='name'>{posts.userName}</p>
-        <p className='timestamp'>{posts.timeStamp}</p>
-        <p className='status'>{posts.status}</p>
-    </div>
-  )
+    <div className="posts-card" key={id}>
+    <p
+      className="name"
+      onClick={() =>
+        navigate("/profile", {
+          state: { id: posts?.userID, email: posts.userEmail },
+        })
+      }
+    >
+      {posts.userName}
+    </p>
+    <p className="timestamp">{posts.timeStamp}</p>
+    <p className="status">{posts.status}</p>
+
+    <LikeButton userId={currentUser?.userID} postId={posts.id} />
+  </div>
+  );
 }
